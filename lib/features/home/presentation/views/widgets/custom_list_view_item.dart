14734f3283fb77b8e0eb9a7488b95ee3024d1665
nada gharib <106126824+nada263204@users.noreply.github.com>
2024-04-30 23:29:02 +0300

    finishing list view

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -5,20 +5,16 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*0.3,
-      width: 150,
-      child: AspectRatio(
-        aspectRatio: 2.8/4,
-        child: Container(
-          width: 150,
-          height: MediaQuery.of(context).size.height*0.25,
-          decoration:  BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image:const DecorationImage(
-              fit: BoxFit.fill,
-            image:AssetImage("assets/images/Poster.png"))),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.8/4,
+      child: Container(
+        width: 150,
+        height: MediaQuery.of(context).size.height*0.25,
+        decoration:  BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image:const DecorationImage(
+            fit: BoxFit.fill,
+          image:AssetImage("assets/images/Poster.png"))),
       ),
     );
   }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -8,14 +8,37 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: Column(
-        children:[
-          CustomAppBar(),
-          CustomListViewItem()
-        ]
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children:[
+            CustomAppBar(),
+            FeaturedBooksListView(),
+          ]
+        ),
       ),
     );
   }
 }
 
+class  FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: CustomListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
 
