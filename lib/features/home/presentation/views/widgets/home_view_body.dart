f7647f042e4b2365f238782c40b6ee955b9bb3ec
nada gharib <106126824+nada263204@users.noreply.github.com>
2024-05-01 02:09:32 +0300

    finishing bestseller listview design

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:book_store_application/core/utils/styles.dart';
 import 'package:book_store_application/features/home/presentation/views/widgets/custom_appbar.dart';
-import 'package:book_store_application/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:book_store_application/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,36 +10,67 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SafeArea(
       child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children:[
-            CustomAppBar(),
-            FeaturedBooksListView(),
-          ]
-        ),
+        padding: EdgeInsets.all(20.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.textStyle20,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSellerListViewItem(),
+        ]),
       ),
     );
   }
 }
 
-class  FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height*0.3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 15,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: CustomListViewItem(),
-          );
-        },
+      height: 140,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: AspectRatio(
+              aspectRatio: 2.6 / 4,
+              child: Container(
+                width: 150,
+                height: MediaQuery.of(context).size.height * 0.25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    image: const DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/images/Poster.png"))),
+              ),
+            ),
+          ),
+          const SizedBox(width: 16,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Movie Name',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: Styles.textStyle20.copyWith(color: Colors.white),),
+              Text('sub title',
+              style: Styles.textStyle16.copyWith(color: Colors.grey),),
+              Text('19',
+              style: Styles.textStyle20.copyWith(color: Colors.white,fontWeight: FontWeight.bold),),            ],
+          )
+        ],
       ),
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:book_store_application/features/splash/presentation/views/screens/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const bookstoreApp());
@@ -15,8 +16,11 @@ class bookstoreApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark()
-          .copyWith(scaffoldBackgroundColor: const  Color.fromARGB(255, 39, 31, 68)),
+          .copyWith(scaffoldBackgroundColor: const  Color.fromARGB(255, 39, 31, 68),
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       home: const SplashView(),
     );
   }
 }
+
+
